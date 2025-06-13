b8ba6f7a83c2cc719c46abbe4960826830830583
wenderf5 <wenderfabianoalves@gmail.com>
2025-06-13 01:31:50 -0300

    feat: button add new chart

--- src/components/addChart/_components/label/index.tsx
+++ src/components/addChart/_components/label/index.tsx
@@ -1,10 +1,19 @@
 import style from './index.module.css';
-import { Plus, ChevronDown, ChevronUp } from 'lucide-react';
+import { ChevronDown, ChevronUp } from 'lucide-react';
 
-export function Label() {
+interface props {
+    isOpen: boolean,
+    setIsOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+export function Label({ isOpen, setIsOpen }: props) {
     return (
-        <p className={style.LabelText}>
-            <ChevronDown /> Adicionar nova gráfico
+        <p
+            className={style.LabelText}
+            onClick={() => setIsOpen(!isOpen)}
+        >
+            {isOpen ? <ChevronUp /> : <ChevronDown />}
+            Adicionar novo gráfico
         </p>
     );
 }
--- src/components/addChart/index.tsx
+++ src/components/addChart/index.tsx
@@ -2,19 +2,21 @@ import style from './index.module.css';
 import { Label } from './_components/label';
 import { Options } from './_components/options';
 import { AnimatePresence, motion } from 'framer-motion';
-import { useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, RootState } from '../../store';
-import { toggle } from '../../store/visibilitySlice';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { AppDispatch, RootState } from '../../store';
+// import { toggle } from '../../store/visibilitySlice';
+import { useState } from 'react';
 
 export function AddChart() {
-    const visible = useSelector((state: RootState) => state.visibility.visible);
-    const dispatch = useDispatch<AppDispatch>();
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+    // const visible = useSelector((state: RootState) => state.visibility.visible);
+    // const dispatch = useDispatch<AppDispatch>();
 
     return (
-        <div onClick={() => dispatch(toggle())} className={style.addChartContainer}>
-            <Label />
+        <div className={style.addChartContainer}>
+            <Label isOpen={isOpen} setIsOpen={setIsOpen} />
             <AnimatePresence>
-                {visible && (
+                {isOpen && (
                     <motion.div
                         initial={{ opacity: 0, height: 0 }}
                         animate={{ opacity: 1, height: "auto" }}
